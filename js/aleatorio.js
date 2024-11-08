47b13134cb114d2383171adafbf225bf7d72764a
izadora oligini <107197685+izadoraoligini3255@users.noreply.github.com>
2024-11-08 09:43:48 -0300

    08/10/2024

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,7 +1,9 @@
- const nomes =["julia", "maria", "Bruna", "Joana", " Felipe", "Gabriel"]
+ const nomes =["julia", "maria", "Bruna", "Joana", " Felipe", "Gabriel"];
  
  
  export function aleatorio(lista){
     const posicao = Math.floor(Math.ramdon()*lista.length);
     return lista[posicao];
 }
+
+export conts nome = aleatorio(nomes);
